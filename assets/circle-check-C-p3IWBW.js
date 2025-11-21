7abe0409337bb42bb8da57b5f776eab96cbda364
Fabricio Brigagao <fb8@illinois.edu>
2025-11-21 17:26:10 -0300

    Fixes and adiciona tags.

--- assets/circle-check-C-p3IWBW.js
+++
@@ -1,7 +0,0 @@
-import{c}from"./index-BmE-4Faz.js";
-/**
- * @license lucide-vue-next v0.548.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=c("circle-check",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);export{e as C};
